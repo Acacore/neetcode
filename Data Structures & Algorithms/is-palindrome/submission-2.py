091f6e0c04ebe6fc53a41b7b501f98684ab48c5a
Mensah Edoh <mensart81@outlook.com>
2026-05-15 14:17:22 +0000

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = [ch.lower() for ch in s if ch.isalnum()]
+        return cleaned == cleaned [::-1]
+    
+        
+        
+        
