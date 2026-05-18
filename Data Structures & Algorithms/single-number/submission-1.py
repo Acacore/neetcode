8933613a732ce074ca1899d7eb7cd24a9de723ae
Mensah Edoh <mensart81@outlook.com>
2026-05-18 11:54:25 +0000

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        from functools import reduce    
+        from operator import xor
+
+        return reduce(xor, nums)
+
+
+
+
+            
+        
+        
